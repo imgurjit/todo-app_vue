e800a02f888d06e06e3a6e1e61f0c542035323e8
Gurjit Singh <gurjitsingh935@gmail.com>
2020-09-06 12:53:23 +0530

    Removed extra code

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <div class="container">
       <AddTodo />
-      <!-- <FilterTodos /> -->
       <Todos />
     </div>
   </div>
@@ -11,13 +10,11 @@
 <script>
 import Todos from "./components/Todos.vue";
 import AddTodo from "./components/AddTodo.vue";
-// import FilterTodos from "./components/FilterTodos.vue";
 export default {
   name: "app",
   components: {
     Todos,
     AddTodo,
-    // FilterTodos,
   },
 };
 </script>
